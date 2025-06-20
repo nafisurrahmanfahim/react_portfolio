f1a4919809816f3b1646ca54b525a0e929bab034
nafisurrahmanfahim <nafisurr93@gmail.com>
2025-06-20 23:04:24 +0600

    demo

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -6,7 +6,7 @@ const HeroPic = () => {
     <div className='h-full flex items-center justify-center'>
         <img src={Profile} alt="" className='max-h-[450px] w-auto rounded-full'/>
         <div className="absolute -z-10 flex justify-center items-center animate-pulse">
-            <FiHexagon className='md:h-[90%] sm:h-[120%] min-h-[600px] sm:w-[550px] md:w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]'/>
+            <FiHexagon className='md:h-[90%] sm:h-[120%] min-h-[600px] sm:w-[500px] md:w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]'/>
         </div>
     </div>
   )
--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -11,7 +11,7 @@ const NavbarMain = () => {
         setMenuOpen(!menuOpen);
     }
     return (
-            <nav className='py-4 md:max-w-[1300px] sm:max-w-[500px] mx-auto px-4 w-full fixed md:left-[50%] md:-translate-x-[50%] z-20 flex gap-4 mt-2'>
+            <nav className='py-4 md:max-w-[1300px] sm:max-w-[450px] mx-auto px-4 w-full fixed md:left-[50%] md:-translate-x-[50%] z-20 flex gap-4 mt-2'>
                 <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0
             .5px] border-orange">
                     <NavbarLogo />
